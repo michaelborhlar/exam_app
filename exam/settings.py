bc26696fbf854f5b1e5d5de90bdc0deae3f38494
mhykeborhlar <michealadefehinti09@gmail.com>
2025-10-07 13:41:51 +0100

    added some files

--- exam/settings.py
+++ exam/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hp4#vi(&d-h1@n9%lh%=eaxxmd#*8istzw8a3*_1#9+_7)l94k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
