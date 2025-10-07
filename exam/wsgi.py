aba5eeaa0703369b5dc6201d12fbe02e0ae48690
mhykeborhlar <michealadefehinti09@gmail.com>
2025-10-07 13:28:26 +0100

    made few changes

--- exam/wsgi.py
+++ exam/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/mhykeborhlar/exam_app'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'exam.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam.settings')
-
 application = get_wsgi_application()
